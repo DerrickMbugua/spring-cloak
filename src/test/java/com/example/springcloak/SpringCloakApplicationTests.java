126c2a2f3645f1ec1c7d7748038ad107269e56fb
“derrick” <derrickmbugua50@gmail.com>
2023-05-04 08:31:24 +0300

    first commit

---
+++ src/test/java/com/example/springcloak/SpringCloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
